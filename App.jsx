a480fa9ddc002326b34dfec24997b45c66b16b6c
NAVEEN143341 <kumarknaveen2580@gmail.com>
2025-11-15 10:23:56 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="p-6 text-xl font-bold bg-gray-100 min-h-screen">
+      Document Summary Assistant (CRA Version with Tailwind)
+    </div>
+  );
+}
